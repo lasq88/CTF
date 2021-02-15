d60d127934151f872050134930b769769e66967a
lasq88 <lasq88@gmail.com>
2021-02-15 23:16:34 +0800

    Add files via upload

---
+++ randomstuff/powershell/encryption.ps1
@@ -0,0 +1,8 @@
+﻿$ct = [system.Text.Encoding]::UTF8.GetBytes("Write 'Congratulations!'");
+[System.IO.MemoryStream] $output = New-Object System.IO.MemoryStream;
+$sr = New-Object System.IO.Compression.DeflateStream($output, [System.IO.Compression.CompressionMode]::Compress);
+$sr.Write($ct, 0, $ct.Length);
+$sr.Close();
+$output.Close();
+$b64 =  [System.Convert]::ToBase64String($output.ToArray());
+Write-Output $b64;
